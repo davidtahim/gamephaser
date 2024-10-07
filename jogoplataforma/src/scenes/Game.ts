caef54bdfc94a47fcd48a4144e041ff17ddae657
David Tahim <davidtahim@gmail.com>
2024-10-07 20:35:52 -0300

    criar a cena sem o collider

--- jogoplataforma/src/scenes/Game.ts
+++ jogoplataforma/src/scenes/Game.ts
@@ -2,44 +2,109 @@ import { Scene } from 'phaser';
 
 var platforms;
 
-export class Game extends Scene
-{
-    constructor ()
-    {
+
+export class Game extends Scene {
+    private platforms?: Phaser.Physics.Arcade.StaticGroup;
+    private player?: Phaser.Physics.Arcade.Sprite;
+    private stars?: Phaser.Physics.Arcade.Group;
+    private bombs?: Phaser.Physics.Arcade.Group;
+
+
+
+
+    constructor() {
         super('Game');
     }
 
-    preload ()
-    {
-    this.load.image('platform', 'assets/platform.png');
-    this.load.image('sky','assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 
-        'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-    );
-        
+    preload() {
+        this.load.image('platform', 'assets/platform.png');
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude',
+            'assets/dude.png',
+            { frameWidth: 32, frameHeight: 48 }
+        );
+
     }
 
-    create ()
-    {
-        this.add.image(400, 300, 'sky');
+    create() {
+        this.createBackgroundImage();
+        this.createPlatforms();
+        this.createPlayer();
+        this.createStars();
+        this.createCollider(this.player, this.platforms, this.stars, this.bombs);
+    
 
-        platforms=this.physics.add.staticGroup();
+    }
+
+
+
+    update() {
+
+    }
+    createBackgroundImage() {
+        this.add.image(400, 300, 'sky');
+    }
+    createPlatforms() {
+
+        platforms = this.physics.add.staticGroup();
         platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
         platforms.create(600, 400, 'ground');
         platforms.create(50, 250, 'ground');
         platforms.create(750, 220, 'ground');
 
-        this.add.image(400, 300, 'star');
-        
-        
     }
-update() {
-    
-}
-    
+
+
+
+    createPlayer() {
+        player = this.physics.add.sprite(100, 450, 'dude');
+
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'dude', frame: 4 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+    }
+
+    createStars() {
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        })
+        this.stars.children.iterate(c => {
+            const child = c as Phaser.Physics.Arcade.Image;
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
+        })
+    }
+
+    createCollider(player,platforms,stars,bombs){
+        this.physics.add.collider(player, platforms);
+        this.physics.add.collider(player, bombs, this.handleHitBomb, undefined, this);
+        this.physics.add.collider(stars, platforms);
+        this.physics.add.collider(bombs, platforms);
+    }
+
+
 }
