d5dab45f41d907d01f5b58a91701da919e400bd7
David Tahim <davidtahim@gmail.com>
2024-10-07 19:59:30 -0300

    adicionou os elementos do jogo

--- jogoplataforma/src/main.ts
+++ jogoplataforma/src/main.ts
@@ -7,14 +7,21 @@ const config: Types.Core.GameConfig = {
     type: AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    
     scale: {
         mode: Scale.FIT,
         autoCenter: Scale.CENTER_BOTH
     },
     scene: [
-        Game
+        new MainGame()
     ]
 };
 
--- jogoplataforma/src/scenes/Game.ts
+++ jogoplataforma/src/scenes/Game.ts
@@ -1,5 +1,7 @@
 import { Scene } from 'phaser';
 
+var platforms;
+
 export class Game extends Scene
 {
     constructor ()
@@ -9,17 +11,7 @@ export class Game extends Scene
 
     preload ()
     {
-        
-    }
-
-    create ()
-    {
-        this.add.image(400, 300, 'sky');
-        this.add.image(400, 300, 'star');
-        
-        
-    }
-update() {
+    this.load.image('platform', 'assets/platform.png');
     this.load.image('sky','assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
@@ -28,6 +20,26 @@ update() {
         'assets/dude.png',
         { frameWidth: 32, frameHeight: 48 }
     );
+        
+    }
+
+    create ()
+    {
+        this.add.image(400, 300, 'sky');
+
+        platforms=this.physics.add.staticGroup();
+        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        platforms.create(600, 400, 'ground');
+        platforms.create(50, 250, 'ground');
+        platforms.create(750, 220, 'ground');
+
+        this.add.image(400, 300, 'star');
+        
+        
+    }
+update() {
+    
 }
     
 }
